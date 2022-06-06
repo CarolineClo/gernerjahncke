1b8418af2ebdaade4c5f1572cf00e5e0a5086b63
Michalina <michalina.oniszczuk@gmail.com>
2022-06-06 18:41:03 +0200

    added fetching on ourproducts.html

--- Website/ourproducts.js
+++ Website/ourproducts.js
@@ -1,39 +1,141 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("collection");
+//const urlParams = new URLSearchParams(window.location.search);
+//const id = urlParams.get("product");
 
 
-let url="http://michalinaoniszczuk.com/examwp/wp-json/wp/v2/product/";
+//let url="http://michalinaoniszczuk.com/examwp/wp-json/wp/v2/product/";
 
-if (collection) {
-    url += `?q={"Collection": "${collection}"}`;
+//if (collection) {
+  //  url += `?q={"Collection": "${collection}"}`;
+
+  window.addEventListener("load", setup);
+  const endpoint = "http://michalinaoniszczuk.com/examwp/wp-json/wp/v2/";
+function setup(){
+    getMaterials();
+    getCategories();
+    getDenmark();
+
+   
+    
 }
 
-console.log(url);
+//console.log(url);
 
-fetch(url)
-.then((res) => res.json())
-.then((data) => showProduct(data));
+//fetch(url)
+//.then((res) => res.json())
+//.then((data) => showProduct(data));
 
 //populating the product page
 
-function showProduct(product) {
-console.log(product)
-document.querySelector(".product_name").textContent = product.product_name;
-document.querySelector(".product_description").textContent = product.content.rendered;
-//document.querySelector("img.otherview").src = `http://michalinaoniszczuk.com/examwp/wp-content/uploads/2022/06/IMG_8143.webp`;
-document.querySelector("img.otherview").alt = product.product_name;
-document.querySelector(".capacity").textContent = `${product.height}ml`;
-document.querySelector(".height").textContent = `Height: ${product.height}mm`;
-document.querySelector(".width").textContent = `Width: ${product.width}mm`;
-document.querySelector(".radius").textContent = `Radius: ${product.radius}mm`;
-document.querySelector(".price").textContent = `${product.price} DKK`;
+/*function showProducts(products) {
+    products.forEach((product) => {
+        console.log(product);
+        const template_material = document.querySelector("#material_button").content;
+        const template_collection = document.querySelector("#collection_button").content;
+        const clone_material = template.cloneNode(true);
+        const clone_collection = template.cloneNode(true);
+
+       /clone_material.querySelector(h2).textContent = product.product_name;
+        clone_collection.querySelector(h2).textContent = category.name;
+    
+        const parent = document.querySelector("main");
+
+        parent.appendChild(clone_collection);
+    });
+
 }
-
-/*
-
-event.listener(onlick)
-(onload)
-
 
 */
 
+function getCategories() {
+    fetch(endpoint + "categories?parent=5")
+    .then(res => res.json())
+    .then(setupCategories);
+    
+}
+
+function setupCategories(catArray) {
+const template = document.querySelector("template#collection_button").content;
+const parentElement = document.querySelector(".countrynav");
+catArray.forEach(cat => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h2").textContent = cat.name;
+    parentElement.appendChild(copy);
+    
+});
+}
+
+function getMaterials() {
+    fetch(endpoint + "categories?parent=14")
+    .then(res => res.json())
+    .then(setupMaterials);
+}
+
+function setupMaterials(matArray) {
+const template = document.querySelector("template#material_button").content;
+const parentElement = document.querySelector(".materialnav");
+matArray.forEach(mat => {
+    const copy = template.cloneNode(true);
+    copy.querySelector("h2").textContent = mat.name;
+    parentElement.appendChild(copy);
+});
+}
+
+function getDenmark() {
+fetch(endpoint + "product?categories=6&_embed")
+.then(res => res.json())
+    .then(setupDenmark);
+}
+
+function setupDenmark(prodArray) {
+    console.log(prodArray);
+    const template = document.querySelector("template#product_card").content;
+    const parentElement = document.querySelector("main");
+    prodArray.forEach(prod => {
+        const copy = template.cloneNode(true);
+        copy.querySelector("img").src = prod._embedded["wp:featuredmedia"][0].source_url;
+        copy.querySelector("h2").textContent = prod.product_name;
+        copy.querySelector("p.price").textContent = prod.price;
+        parentElement.appendChild(copy);
+    });
+    }
+
+
+    function getIceland() {
+        fetch(endpoint + "product?categories=8&_embed")
+        .then(res => res.json())
+            .then(setupIceland);
+        }
+        
+        function setupIceland(prodArray) {
+            console.log(prodArray);
+            const template = document.querySelector("template#product_card").content;
+            const parentElement = document.querySelector("main");
+            prodArray.forEach(prod => {
+                const copy = template.cloneNode(true);
+                copy.querySelector("img").src = prod._embedded["wp:featuredmedia"][0].source_url;
+                copy.querySelector("h2").textContent = prod.product_name;
+                copy.querySelector("p.price").textContent = prod.price;
+                parentElement.appendChild(copy);
+            });
+            }
+
+            function getJapan() {
+                fetch(endpoint + "product?categories=7&_embed")
+                .then(res => res.json())
+                    .then(setupJapan);
+                }
+                
+                function setupJapan(prodArray) {
+                    console.log(prodArray);
+                    const template = document.querySelector("template#product_card").content;
+                    const parentElement = document.querySelector("main");
+                    prodArray.forEach(prod => {
+                        const copy = template.cloneNode(true);
+                        copy.querySelector("img").src = prod._embedded["wp:featuredmedia"][0].source_url;
+                        copy.querySelector("h2").textContent = prod.product_name;
+                        copy.querySelector("p.price").textContent = prod.price;
+                        parentElement.appendChild(copy);
+                    });
+                    }
+
+            
